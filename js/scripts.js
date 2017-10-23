f20c1854a0fc2f86f7f87b520ea712ebb4d9feb8
kylo666 <32401843+kylo666@users.noreply.github.com>
2017-10-23 19:46:38 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var list = document.getElementById('list')
+
+add.addEventListener('click' , function('li') {
+});
+
+list.addEventListener('click' , function('li') {
+});
+element.innerHTML = 'li';
+
+console.log(element[li].innerText);
+
+list.appendChild(element);
