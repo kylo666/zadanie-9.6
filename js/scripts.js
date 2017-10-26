f492d307e251c169a96ddd8a1454fdea7441b266
kylo666 <32401843+kylo666@users.noreply.github.com>
2017-10-26 20:23:36 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
-var list = document.getElementById('list')
+var list = document.getElementById('list');
+var add = document.getElementById('add');
 
-add.addEventListener('click' , function('add') {
-});
+add.addEventListener('click' , function() {'Dodaj'});
 
-list.addEventListener('click' , function('list') {
-});
+list.addEventListener('click' , function() {'item 0'});
 element.innerHTML = 'li';
 
 console.log(element[li].innerText);
 
-list.appendChild(element);
+list.appendChild(li);
